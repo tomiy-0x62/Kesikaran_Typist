92069b159e941e72ea57e2c10c0cc8d1e729b471
tomiy <tomiy@tomiylab.com>
2020-12-19 18:47:27 +0900

    Initial Commit

---
+++ Kesikaran_Typist/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Kesikaran_Typist
+//
+//  Created by Tomishige Ryosuke on 2020/12/19.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
