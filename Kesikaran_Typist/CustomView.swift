c941af9ec882ff478427e71c2423c40d59e2e005
tomiy <tomiy@tomiylab.com>
2021-01-22 23:19:04 +0900

    deleted: comment

--- Kesikaran_Typist/CustomView.swift
+++ Kesikaran_Typist/CustomView.swift
@@ -25,33 +25,11 @@ import QuartzCore
  }*/
 
 class CustomView: NSView, CALayerDelegate {
-    /*
-    var bgLayer     = CALayer()
-    
-    override func awakeFromNib() {
-        self.wantsLayer = true  // Layer Backed Viewにする
-        self.layer?.delegate = self
-        self.layer?.addSublayer(bgLayer)
-    }*/
     
     // KeyDownに対応するためのクラス
     override func performKeyEquivalent(with event: NSEvent) -> Bool {
         return true
     }
-    /*
-    override func layout() {
-        // 諸々レイアウト
-        super.layout()
-        
-        bgLayer.frame = CGRect(x: 0, y: 0, width: self.frame.width, height: self.frame.height)
-        bgLayer.backgroundColor = CGColor.init(red: 0.0, green: 1.0, blue: 0.0, alpha: 1.0)
-        bgLayer.cornerRadius = 15
-        bgLayer.borderWidth = 10
-        bgLayer.borderColor = CGColor.black
-    }*/
     
 }
 
-// 背景に図形を描画したい
-// https://clrmemory.com/programming/swift/cocoa-nsgraphicscontext-draw/
-// https://qiita.com/Kyome/items/258cc0c1ee1291b0f207
