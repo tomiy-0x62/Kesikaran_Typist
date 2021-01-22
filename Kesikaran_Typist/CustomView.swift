e0d6adada0dd79bbe10eb7ffc94263c527ce492e
tomiy <tomiy@tomiylab.com>
2021-01-22 23:12:06 +0900

    added: keybord background

--- Kesikaran_Typist/CustomView.swift
+++ Kesikaran_Typist/CustomView.swift
@@ -6,68 +6,52 @@
 //
 
 import Cocoa
+import QuartzCore
 
-extension NSView {
+/*
+ extension NSView {
+ 
+ @IBInspectable var backgroundColor: NSColor? {
+ get {
+ guard let layer = layer, let backgroundColor = layer.backgroundColor else {return nil}
+ return NSColor(cgColor: backgroundColor)
+ }
+ set {
+ wantsLayer = true
+ layer?.backgroundColor = newValue?.cgColor
+ }
+ }
+ 
+ }*/
 
-    @IBInspectable var backgroundColor: NSColor? {
-        get {
-            guard let layer = layer, let backgroundColor = layer.backgroundColor else {return nil}
-            return NSColor(cgColor: backgroundColor)
-        }
-        set {
-            wantsLayer = true
-            layer?.backgroundColor = newValue?.cgColor
-        }
-    }
-
-}
-
-class CustomView: NSView {
+class CustomView: NSView, CALayerDelegate {
+    /*
+    var bgLayer     = CALayer()
+    
+    override func awakeFromNib() {
+        self.wantsLayer = true  // Layer Backed Viewにする
+        self.layer?.delegate = self
+        self.layer?.addSublayer(bgLayer)
+    }*/
     
     // KeyDownに対応するためのクラス
     override func performKeyEquivalent(with event: NSEvent) -> Bool {
         return true
     }
-    
-    required init?(coder decoder: NSCoder) {
-            super.init(coder: decoder)
-
-            wantsLayer = true
-            layer?.backgroundColor = NSColor.gray.cgColor
+    /*
+    override func layout() {
+        // 諸々レイアウト
+        super.layout()
         
-        var views = NSView()
-        views = DrawRectangle(frame: NSRect(x: 5, y: 5, width: self.bounds.width - 10, height: self.bounds.height - 10))
-        self.addSubview(views)
-        
-        }
+        bgLayer.frame = CGRect(x: 0, y: 0, width: self.frame.width, height: self.frame.height)
+        bgLayer.backgroundColor = CGColor.init(red: 0.0, green: 1.0, blue: 0.0, alpha: 1.0)
+        bgLayer.cornerRadius = 15
+        bgLayer.borderWidth = 10
+        bgLayer.borderColor = CGColor.black
+    }*/
     
 }
 
 // 背景に図形を描画したい
 // https://clrmemory.com/programming/swift/cocoa-nsgraphicscontext-draw/
 // https://qiita.com/Kyome/items/258cc0c1ee1291b0f207
-
-class DrawRectangle: NSView{
-    override func draw(_ dirtyRect: NSRect) {
-        super.draw(dirtyRect)
-        if let context = NSGraphicsContext.current?.cgContext {
-                    context.beginPath()
-
-                    // 線の幅の設定
-                    context.setLineWidth(2.0)
-                    // 線の色の設定
-                    context.setStrokeColor(CGColor.black)
-                    // 塗りつぶし色の設定
-                    context.setFillColor(CGColor.white)
-                    // 描画色のモード設定
-                    context.setBlendMode(CGBlendMode.multiply)
-
-                    // 中塗りパスの描画
-                    context.fillPath()
-                    //　パスのアウトラインの描画
-                    context.strokePath()
-                }
-    }
-}
-
-
---
+++ Kesikaran_Typist/KeybordBack.swift
@@ -0,0 +1,45 @@
+//
+//  KeybordBack.swift
+//  Kesikaran_Typist
+//
+//  Created by Tomishige Ryosuke on 2021/01/22.
+//
+
+
+import Cocoa
+import QuartzCore
+
+class KeybordBack: NSView, CALayerDelegate {
+    
+    var bgLayer     = CALayer()
+    
+    override func awakeFromNib() {
+        self.wantsLayer = true  // Layer Backed Viewにする
+        self.layer?.delegate = self
+        self.layer?.addSublayer(bgLayer)
+    }
+    
+    // KeyDownに対応するためのクラス
+    override func performKeyEquivalent(with event: NSEvent) -> Bool {
+        return true
+    }
+    
+    override func layout() {
+        // 諸々レイアウト
+        super.layout()
+        
+        bgLayer.frame = CGRect(x: 0, y: 0, width: self.frame.width, height: self.frame.height)
+        bgLayer.backgroundColor = CGColor.init(red: 0.7294, green: 0.9568, blue: 1.0, alpha: 0.8)
+        bgLayer.cornerRadius = 10
+        bgLayer.borderWidth = 2
+        bgLayer.borderColor = CGColor.init(gray: 0.7, alpha: 1.0)
+    }
+    
+    
+    
+    // 背景に図形を描画したい
+    // https://clrmemory.com/programming/swift/cocoa-nsgraphicscontext-draw/
+    // https://qiita.com/Kyome/items/258cc0c1ee1291b0f207
+    
+    
+}
