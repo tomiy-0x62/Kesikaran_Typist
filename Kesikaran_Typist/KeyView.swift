ce097199b4e79ff866b76f2b4c3be398d03d6371
tomiy <tomiy@tomiylab.com>
2021-01-24 10:38:15 +0900

    adding: change key back

--- Kesikaran_Typist/KeyView.swift
+++ Kesikaran_Typist/KeyView.swift
@@ -22,25 +22,25 @@ class KeyView: NSImageView {
     }
     
     func  turnOn() {
-        var imageLayer: CALayer = CALayer()
+        let imageLayer = CALayer()
+        let keyImage = self.image
+        print(keyImage!)
         bgLayer.frame = CGRect(x: 0, y: 0, width: self.frame.width, height: self.frame.height)
-        bgLayer.backgroundColor = CGColor.init(red: 1.0, green: 0.7058, blue: 0.4078, alpha: 0.8)
+        bgLayer.backgroundColor = CGColor.init(red: 1.0, green: 0.7058, blue: 0.4078, alpha: 1.0)
         bgLayer.cornerRadius = 5
+        // bgLayer.zPosition = -1
         self.layer?.addSublayer(bgLayer)
-        self.layer?.sublayers?.forEach {
-            print(type(of: $0))
-            if type(of: $0) != CALayer.self {
-                imageLayer = $0
-                $0.removeFromSuperlayer()
-            }
-        }
+        imageLayer.contents = keyImage!
+        // imageLayer.zPosition = 0
         self.layer?.addSublayer(imageLayer)
     }
     
     func turnOff() {
         self.layer?.sublayers?.forEach {
+            print(type(of: $0))
             if type(of: $0) == CALayer.self {
                 $0.removeFromSuperlayer()
+                // print("hello")
             }
         }
     }
